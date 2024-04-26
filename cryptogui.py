ff747217515816207d1e66a684c3fc6d30c9386c
Willski2K <wille@ernell.se>
2024-04-26 14:45:18 +0200

    eggs

--- cryptogui.py
+++ cryptogui.py
@@ -2,6 +2,16 @@ import PySimpleGUI as sg
 import pandas as pd
 import numpy as np
 
+def est():
+    layout = [[sg.Text('Det finns 3 easter eggs på detta programmet och kanske till och med något mer ;) , Försök hitta dem utan att kolla koden')], [sg.Button('OK')]]
+    window = sg.Window('Popup', layout, keep_on_top=True)
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Jag lovar att hitta dem':
+            break
+    
+    window.close()
+
 df = pd.read_csv('CryptoGui/coin_gecko_2022-03-17.csv')
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -11,8 +21,8 @@ default_font = 'Bold'
 
 layout = [
     [sg.Text("CryptoPriceGui")],
-    [sg.Button("Randomize"), sg.Button("SortName"), sg.Button("SortPrice"), sg.Button("Refresh Price")],
-    [sg.InputText("Filter1"), sg.InputText("Filter2")],
+    [sg.Button("Randomize"), sg.Button("SortName"), sg.Button("SortPrice"), sg.Button("Refresh Price"), sg.Button("EasterEggs")],
+    [sg.InputText("Search*")],
     [sg.Table(values=df.values.tolist(), headings=df.columns.tolist(), display_row_numbers=False, auto_size_columns=False, col_widths=[20, 10], vertical_scroll_only=True, key='-TABLE-')]
 ]
 window = sg.Window("CryptoGUI", layout)
@@ -43,5 +53,6 @@ while True:
         window['-TABLE-'].update(values=df.values.tolist())
     if event == "Randomize":
         np.random.shuffle(df.values)
-        
         window['-TABLE-'].update(values=df.values.tolist())
+    if event == "EasterEggs":
+        est()
